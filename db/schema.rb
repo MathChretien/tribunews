08d65a83d9180ec44175826ecd2b1ca286d5ff0a
bo <bobppamail@gmail.com>
2019-03-12 16:03:49 +0100

    this is the point where i start working and everything works ....

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_03_12_134507) do
-
+ActiveRecord::Schema.define(version: 2019_03_12_135745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +57,9 @@ ActiveRecord::Schema.define(version: 2019_03_12_134507) do
     t.string "address"
     t.string "description"
     t.date "subscribed_on"
+    t.bigint "user_id"
     t.string "photo"
+    t.index ["user_id"], name: "index_tribes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -70,11 +70,25 @@ ActiveRecord::Schema.define(version: 2019_03_12_134507) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "firstName"
+    t.string "lastName"
     t.string "first_name"
     t.string "last_name"
     t.bigint "tribe_id"
     t.string "photo"
+    t.string "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer "invitation_limit"
+    t.string "invited_by_type"
+    t.bigint "invited_by_id"
+    t.integer "invitations_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
+    t.index ["invitations_count"], name: "index_users_on_invitations_count"
+    t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
+    t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by_type_and_invited_by_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["tribe_id"], name: "index_users_on_tribe_id"
   end
@@ -84,5 +98,6 @@ ActiveRecord::Schema.define(version: 2019_03_12_134507) do
   add_foreign_key "pages", "newspapers"
   add_foreign_key "pictures", "boxes"
   add_foreign_key "pictures", "newspapers"
+  add_foreign_key "tribes", "users"
   add_foreign_key "users", "tribes"
 end
