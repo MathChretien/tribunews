2b3353c6a52cb1a5c9820653eda31aad4f7824d1
Dany Buts <danybu.danybu@gmail.com>
2019-03-13 22:45:50 +0100

    single file upload works

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,16 +5,23 @@ class PicturesController < ApplicationController
   end
 
   def create
-    puts ">>>>>>"+current_user.tribe.current_newspaper.id
-    # newspaper = current_user.tribe.newspaper
-    # @new_picture = Picture.create!(picture_params)
-    raise
+    # puts current_user.tribe.current_newspaper.id
+    @newspaper = current_user.tribe.current_newspaper
+    new_params = picture_params
+    new_params[:newspaper] = @newspaper
+    @picture = Picture.create!(new_params)
+    if @picture.save!
+      redirect_to newspapers_show_path
+    else
+      render :newspapers_show
+    end
+
   end
 
   private
 
   def picture_params
-    params.require(:picture).permit(:box_id, :newspaper_id, :photo, :created_at, :updated_at)
+    params.require(:picture).permit(:box_id, :newspaper_id, :photo, :photo_cache, :created_at, :updated_at)
   end
 
 end
--- app/javascript/packs/pictureUpload.js
+++ app/javascript/packs/pictureUpload.js
@@ -1,11 +1,10 @@
 const pictureUploadInit = () => {
 
   const uploadButton = document.getElementById("picture_photo");
-  uploadButton.addEventListener('change', (event) => {
+  if (uploadButton) uploadButton.addEventListener('change', (event) => {
     const form = event.currentTarget.parentElement.parentElement;
     console.log(form);
     form.submit();
-    console.log('yeah');
   });
 };
 
