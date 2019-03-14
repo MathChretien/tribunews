0ba55de7d6046954780874f7fa5cefdc29b97a60
bo <bobppamail@gmail.com>
2019-03-14 19:30:45 +0100

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   belongs_to :tribe, optional: true
   accepts_nested_attributes_for :tribe
+  before_create :add_tribe
 
+  def add_tribe
+   if self.tribe.nil? && self.invitation_accepted_at.nil?
+    tribe =  Tribe.create()
+    self.tribe = tribe
+    end
+  end
 end
