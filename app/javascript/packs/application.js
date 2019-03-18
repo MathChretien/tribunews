b3db4f5ec6c6546e65e03ef3a33fa1ac6e3ac549
Dany Buts <danybu.danybu@gmail.com>
2019-03-18 10:29:36 +0100

    js layoutpicker makes patch request

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 import "bootstrap";
 import {pictureUploadInit} from './pictureUpload';
-import {changePageInit} from './changePage';
+import {changePageInit, getPageNr} from './changePage';
 import {countdownInit} from './countdown';
 import {fullscreenInit} from './fullscreen';
 import {dragAndDropInit} from './dragAndDrop';
