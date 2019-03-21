618be6dac321143bdf16a155503f268fd1fefc3d
Dany Buts <danybu.danybu@gmail.com>
2019-03-21 23:02:34 +0100

    debugging2

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -11,7 +11,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   version :thumbnail do
-    cloudinary_transformation gravity: :face, width: 300, crop: :thumb, zoom: 0.2, height: 250
+    cloudinary_transformation gravity: :face, width: 250, crop: :thumb, zoom: 0.2, height: 200
   end
 
   version :layout_01 do
@@ -23,6 +23,11 @@ class PhotoUploader < CarrierWave::Uploader::Base
     {effect: "art:fes", opacity:80}, {effect: "gradient_fade:.05,y_-0.1,b_black"}]
   end
 
+  version :fx do
+    cloudinary_transformation transformation: [{gravity: :face, crop: :fill, zoom: 0.1},
+     { effect: "art:peacock" }, {effect: "colorize:2", color: "yellow" }]
+  end
+
   version :layout_02 do
     cloudinary_transformation gravity: :face, width: 500, height: 550, crop: :fill, zoom: 0.1
   end
@@ -32,15 +37,15 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   version :layout_04 do
-    cloudinary_transformation gravity: :face, width: 500, height: 380, crop: :fill, zoom: 0.1
+    cloudinary_transformation gravity: :face, width: 500, height: 300, crop: :fill, zoom: 0.1
   end
 
   version :layout_05 do
-    cloudinary_transformation gravity: :face, width: 250, height: 320, crop: :thumb, zoom: 0.1
+    cloudinary_transformation gravity: :face, width: 250, height: 320, crop: :fill, zoom: 0.1
   end
 
   version :layout_06 do
-    cloudinary_transformation gravity: :face, width: 240, height: 320, crop: :thumb, zoom: 0.05
+    cloudinary_transformation gravity: :face, width: 240, height: 320, crop: :fill, zoom: 0.1
   end
 
 
